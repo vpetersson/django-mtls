9734d1a1e4fc80fd3e78e8cc9c1c1d28bd1f510f
Viktor Petersson <vpetersson@wireload.net>
2019-11-26 18:21:58 +0000

    Fixes structure

--- app/mtls/urls.py
+++ app/mtls/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('auth/', include('mtls_auth.urls')),
+    path('api/', include('mtls_auth.urls')),
     path('admin/', admin.site.urls),
 ]
--- app/mtls_auth/urls.py
+++ app/mtls_auth/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/', views.index, name='index'),
+    path('/', views.index, name='index'),
 ]
